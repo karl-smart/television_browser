f058b657beba6014e80643c1481d3d69e2f63aa4
Karl Smart <you@example.com>
2023-12-05 17:46:10 +0100

    Added markup and fetching capabilities in channel-list component

--- src/app/components/channel-list/channel-list.component.ts
+++ src/app/components/channel-list/channel-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { GetChannelsService } from 'src/app/services/get.channels.service';
 
 @Component({
   selector: 'app-channel-list',
   templateUrl: './channel-list.component.html',
-  styleUrls: ['./channel-list.component.scss']
+  styleUrls: ['./channel-list.component.scss'],
 })
 export class ChannelListComponent {
+  constructor(private getChannels: GetChannelsService) {
+    getChannels.all().subscribe((res) => {
+      this.channels = res.items;
+    });
+  }
 
+  channels: any;
 }
