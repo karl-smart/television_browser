9ad6a4f3fda30513b9d0b16475e87780e103421a
Karl Smart <you@example.com>
2023-12-05 17:27:00 +0100

    Added channel-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelListComponent } from './components/channel-list/channel-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelListComponent
   ],
   imports: [
     BrowserModule,
