b4a5774beb6866513d7df88be8f19a807cac4a4f
Karl Smart <you@example.com>
2023-12-06 21:09:52 +0100

    Added routes for the components and configured app component to display router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChannelDetailsComponent } from './components/channel-details/channel-details.component';
+import { ChannelListComponent } from './components/channel-list/channel-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChannelListComponent },
+  {
+    path: 'channel/:id',
+    component: ChannelDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
