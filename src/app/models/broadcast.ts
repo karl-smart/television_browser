e29c59593add2af47b1be8ba275bc2c2249be5da
Karl Smart <you@example.com>
2023-12-06 21:15:02 +0100

    Added model Broadcast

---
+++ src/app/models/broadcast.ts
@@ -0,0 +1,10 @@
+export interface Broadcast {
+  channelId: string;
+  endDate: string;
+  endTimeInDefaultTimeZone: string;
+  id: string;
+  item: any;
+  live: boolean;
+  startDate: string;
+  startTimeInDefaultTimeZone: string;
+}
